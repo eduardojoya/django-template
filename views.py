d0f496a77d622b43f79d01352e187b6d4e410725
eduardojoya <ebjoya@gmail.com>
2019-03-09 17:51:10 -0800

    cleaned up views.py

--- views.py
+++ views.py
@@ -8,22 +8,18 @@ def index(request):
 
 
 def about(request):
-#    index_html = open('content/index.html').read()
     context = {}
     return render(request, 'index.html', context)
         
 def resume(request):
-#    resume_html = open('content/resume.html').read()
     context = {}
     return render(request, 'resume.html', context)
     
 def interests(request):
-#    interests_html = open('content/interests.html').read()
     context ={}
     return render(request, 'interests.html', context)
     
 def education(request):
-#    education_html = open('content/education.html').read()
     context = {}
     return render(request, 'education.html', context)
     
